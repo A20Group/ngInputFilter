b30b8f64d8586df1d6d41a26e0f5574064912ac9
AmirHossein Abdollahzadeh <A20Group@yahoo.com>
2019-09-13 19:04:00 +0430

    nullable bug fixed.
    nullable bug fixed.

--- src/ngInputFilterAlphabet.js
+++ src/ngInputFilterAlphabet.js
@@ -1,7 +1,7 @@
 class ngInputFilterAlphabetClass {
     constructor() {
         this.require = 'ngModel',
-        this.restrict = 'A'
+            this.restrict = 'A'
     }
 
     link(scope, element, attrs, ngModelCtrl) {
@@ -10,42 +10,66 @@ class ngInputFilterAlphabetClass {
         if (type == "englishAlphabet") {
             let regex = /^[a-z]*$/i;
             ngModelCtrl.$validators.inputFilterAlphabet = function (value) {
-                return regex.test(value);
+                if (value) {
+                    return regex.test(value);
+                } else {
+                    return true;
+                }
             };
         }
 
         if (type == "english") {
             let regex = /^[0-9a-z]*$/i;
             ngModelCtrl.$validators.inputFilterAlphabet = function (value) {
-                return regex.test(value);
+                if (value) {
+                    return regex.test(value);
+                } else {
+                    return true;
+                }
             };
         }
 
         if (type == "persianAlphabet") {
             let regex = /^[\u0600-\u06EF\u06FA-\u06FF\s]+$/;
             ngModelCtrl.$validators.inputFilterAlphabet = function (value) {
-                return regex.test(value);
+                if (value) {
+                    return regex.test(value);
+                } else {
+                    return true;
+                }
             };
         }
 
         if (type == "persian") {
             let regex = /^[\u0600-\u06FF\s]+$/;
             ngModelCtrl.$validators.inputFilterAlphabet = function (value) {
-                return regex.test(value);
+                if (value) {
+                    return regex.test(value);
+                } else {
+                    return true;
+                }
             };
         }
 
         if (type == "persianWithEnglishNumber") {
             let regex = /^[\u0600-\u06FF0-9\s]+$/;
             ngModelCtrl.$validators.inputFilterAlphabet = function (value) {
-                return regex.test(value);
+                if (value) {
+                    return regex.test(value);
+                } else {
+                    return true;
+                }
             };
         }
 
         if (type == "persianAndEnglishAlphabet") {
             let regex = /^[\u0600-\u06EF\u06FA-\u06FFa-z\s]+$/;
             ngModelCtrl.$validators.inputFilterAlphabet = function (value) {
-                return regex.test(value);
+                if (value) {
+                    return regex.test(value);
+                } else {
+                    return true;
+                }
             };
         }
     }
